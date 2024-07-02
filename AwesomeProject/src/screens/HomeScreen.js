a52bf50fad617121e84660ffa4363936d7690ec5
Tharaa17 <124571050+Tharaa17@users.noreply.github.com>
2024-07-03 01:38:39 +0530

    HomeChange1

--- AwesomeProject/src/screens/HomeScreen.js
+++ AwesomeProject/src/screens/HomeScreen.js
@@ -8,14 +8,13 @@ const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
-        
         <View style={styles.welcomeContainer}>
           <View style={styles.welcomeTextContainer}>
             <Text style={styles.welcomeText}>Hello Mahesh,</Text>
             <Text style={styles.welcomeText}>Welcome Back!</Text>
             <Text style={styles.dateText}>Monday, 10 May</Text>
           </View>
-          <Image  source={require('../../assets/img/Pprofile.jpg')} style={styles.profileImage} />
+          <Image source={require('../../assets/img/Pprofile.jpg')} style={styles.profileImage} />
         </View>
         <Svg height="80" width={width} viewBox={`0 0 ${width} 10`} style={styles.svg}>
           <Path d={`M0,0 Q${width / 2},50 ${width},0 V50 H0 Z`} fill="#fff" />
@@ -23,21 +22,24 @@ const HomeScreen = ({ navigation }) => {
       </View>
       <View style={styles.cardContainer}>
         <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('NewFineScreen')}>
-          <Image  style={styles.cardImage} />
-          <Text style={styles.cardText}>New Fine</Text>
+          <View style={styles.cardContent}>
+            <Text style={styles.cardText}>New Fine</Text>
+            <Image source={require('../../assets/img/newfine.jpg')} style={styles.cardImage} />
+          </View>
           <View style={styles.button}>
             <Text style={styles.buttonText}>Add</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('FineHistoryScreen')}>
-          <Image  style={styles.cardImage} />
-          <Text style={styles.cardText}>Fine History</Text>
+          <View style={styles.cardContent}>
+            <Text style={styles.cardText}>Fine History</Text>
+            <Image source={require('../../assets/img/fineHistory.jpg')} style={styles.cardImage} />
+          </View>
           <View style={styles.button}>
             <Text style={styles.buttonText}>View</Text>
           </View>
         </TouchableOpacity>
       </View>
-      
     </SafeAreaView>
   );
 };
@@ -55,7 +57,6 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 20,
     overflow: 'hidden',
   },
-  
   welcomeContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -64,7 +65,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginTop: 20,
     marginBottom: 20,
-    marginLeft:10,
+    marginLeft: 10,
   },
   welcomeTextContainer: {
     flex: 1,
@@ -77,13 +78,13 @@ const styles = StyleSheet.create({
   dateText: {
     color: '#fff',
     marginTop: 10,
-    fontSize:24,
+    fontSize: 24,
   },
   profileImage: {
     width: 90,
     height: 90,
     borderRadius: 40,
-    marginRight:20,
+    marginRight: 20,
   },
   svg: {
     position: 'absolute',
@@ -91,40 +92,54 @@ const styles = StyleSheet.create({
   },
   cardContainer: {
     flex: 1,
-    paddingHorizontal: 30,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
     marginTop: 20,
   },
   card: {
     backgroundColor: '#00C6FF',
     marginVertical: 10,
-    padding: 5,
+    padding: 20,
     borderRadius: 15,
-    
+  },
+  cardContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   cardImage: {
-    width: 100,
-    height: 100,
-    marginBottom: 10,
+    width: 120, // Increased width
+    height: 120, // Increased height
+    marginRight: 10,
+    marginLeft:10,
+    marginBottom:-30,
+    
   },
   cardText: {
     color: '#fff',
-    fontSize: 18,
+    fontSize: 30,
     fontWeight: 'bold',
-    textAlign:'left',
+    textAlign: 'left',
+    marginLeft: 10,
+    marginBottom: 10,
+    flex: 1, // Allow text to take up remaining space
   },
   button: {
     marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
     backgroundColor: '#fff',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 20,
+    paddingHorizontal: 30, // Reduced padding to make the button smaller
+    paddingVertical: 5, // Reduced vertical padding as well
+    borderRadius: 10, // Adjusted border radius to match smaller button size
+    alignSelf: 'flex-start', // Align button to the start
   },
   buttonText: {
     color: '#00C6FF',
     fontWeight: 'bold',
-    textAlign:'left',
+    textAlign: 'left',
+    fontSize: 18,
   },
- 
 });
 
 export default HomeScreen;
